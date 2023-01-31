a82ec85babe4dee04608969b3fd519dc6439de9b
Sushmita <sushmitakumari363@gmail.com>
2023-01-31 10:48:39 +0530

    total price setup

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -3,14 +3,16 @@ import "./styles/cart.css";
 
 export const Cart = (props) => {
   const { selectedData, onAdd, onRemove } = props;
-  {
-    console.log(selectedData);
-  }
+  const itemPrice = selectedData.reduce((a, c) => a + c.price * c.qty, 0);
+  const taxPrice = itemPrice * 0.14;
+  const shippingPrice = itemPrice > 20000 ? 0 : 50;
+  const totalPrice = itemPrice + taxPrice + shippingPrice;
+
   return (
     <div className="cart">
       <h2 className="cartHeader">Cart Items</h2>
       <div className="productDetails">
-        {selectedData.length == 0 && <div>Cart is empty</div>}
+        {selectedData.length === 0 && <div>Cart is empty</div>}
       </div>
       {selectedData.map((item) => (
         <div key={item.id} className="row">
@@ -28,6 +30,30 @@ export const Cart = (props) => {
           </div>
         </div>
       ))}
+      {selectedData.length !== 0 && (
+        <>
+          <hr></hr>
+
+          <div className="shippingPrice">
+            <div>Item Price</div>
+            <div>${itemPrice}</div>
+          </div>
+
+          <div className="shippingPrice">
+            <div>Tax Price</div>
+            <div>${taxPrice.toFixed(2)}</div>
+          </div>
+          <div className="shippingPrice">
+            <div>Shipping Price</div>
+            <div>${shippingPrice}</div>
+          </div>
+          <hr></hr>
+          <div className="shippingPrice">
+            <div>Total Price</div>
+            <div>${totalPrice}</div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
--- src/components/homePage.jsx
+++ src/components/homePage.jsx
@@ -20,12 +20,24 @@ export const HomePage = () => {
       setSelectedData([...selectedData, { ...productDetail, qty: 1 }]);
     }
   };
+  const onRemove = (productDetail) => {
+    const exist = selectedData.find((x) => x.id === productDetail.id);
+    if (exist.qty === 1) {
+      setSelectedData(selectedData.filter((x) => x.id !== productDetail.id));
+    } else {
+      setSelectedData(
+        selectedData.map((x) =>
+          x.id === productDetail.id ? { ...exist, qty: exist.qty - 1 } : x
+        )
+      );
+    }
+  };
   return (
     <div className="header">
       <Nav />
       <div className="main">
         <Product onAdd={onAdd} productDetails={productDetails} />
-        <Cart onAdd={onAdd} selectedData={selectedData} />
+        <Cart onRemove={onRemove} onAdd={onAdd} selectedData={selectedData} />
       </div>
     </div>
   );
